3fc20742935f81253e2449ce3342ccfe4d6953a3
Marcin Waszak <M.Waszak@stud.elka.pw.edu.pl>
2017-05-23 10:28:24 +0200

    Add fstream header

--- src/common/Connection.cpp
+++ src/common/Connection.cpp
@@ -14,6 +14,7 @@
 #include <sys/socket.h>
 
 #include <iostream> // TODO remove line
+#include <fstream>
 
 static int Resolve(const string &address, addrinfo** info) {
     int result = getaddrinfo(address.c_str(), NULL, NULL, info);
