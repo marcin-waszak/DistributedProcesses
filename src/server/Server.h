9bd71c409afdf7c06897f4f68b98c068b9959c02
Marcin Waszak <M.Waszak@stud.elka.pw.edu.pl>
2017-05-24 22:07:49 +0200

    Added multithreading to server

--- src/server/Server.h
+++ src/server/Server.h
@@ -17,6 +17,9 @@
 #include <unistd.h>
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <thread>
+#include <memory>
+#include <pthread.h>
 
 namespace po = boost::program_options;
 
@@ -24,19 +27,31 @@ using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
+using std::make_pair;
+using std::make_unique;
+using std::unique_ptr;
+using std::thread;
+using std::ostringstream;
+using std::map;
+using std::vector;
 
 class Server {
 public:
   void GetArguments(int argc, char** argv);
   bool ServerLoop();
+  bool CleanThreads();
 
 private:
-  void ExecCmd(Connection& connection);
+  void ThreadFunc(int connect_fd);
+  bool ExecCmd(Connection& connection);
 
   string address_str_;
   int port_;
   fs::path images_path_;
 //  also serialize this vector
-  std::vector<ProcessImage> process_images_;
+  vector<ProcessImage> process_images_;
+
+  map<int, unique_ptr<thread>> threads_;
+  vector<int> threads_closed_;
 };
 
