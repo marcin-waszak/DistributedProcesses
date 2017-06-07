315137d295b4be75dd4f3eb4e44999bf2bfaf50e
Damian Bułak <damianxb3@gmail.com>
2017-06-07 21:20:41 +0200

    Change wireshark plugin

--- wireshark-plugin/pRoman.lua
+++ wireshark-plugin/pRoman.lua
@@ -4,65 +4,72 @@ function proman_proto.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = "PROMAN"
     local subtree = tree:add(proman_proto, buffer(), "PRoman Protocol")
 
-	local function get_next_arg(line)
-		local f_space_index = string.find(line, " ")
-		if f_space_index then
-			return string.sub(line, 1, f_space_index - 1), string.sub(line, f_space_index + 1)
-		end
-		return line, nil
+    local offset = 0
+
+	-- cut-off packet
+    if buffer:len() ~= buffer:reported_len() then
+		return
 	end
 
-	local function add_sub_arg(line, offset, name)
-		arg, line = get_next_arg(line)
-		subtree:add(buffer(offset + 1, arg:len()), name..arg)
-		offset = offset + 1 + arg:len()
-		return line, offset
+	local cmd_length = buffer(offset,4)
+	subtree:add(buffer(offset, 4), "Command length: "..cmd_length:le_uint())
+	offset = offset + 4
+
+	if buffer:len() < offset + cmd_length:le_uint() then
+		pinfo.desegment_len = cmd_length:le_uint()
+		return
 	end
-
-	local get_next_line = buffer():string():gmatch('[^\n]+')
-
-	while true do
-		local line = get_next_line()
-
-		if line == nil then
-			return
-		end
-
-		local arg, line = get_next_arg(line)
-		local offset = 0
 	
-		if arg == nil then
-			return
-		end
+	local command = buffer(offset,cmd_length:le_uint())
+	subtree:add(buffer(offset,cmd_length:le_uint()), "Command: "..command:string())
+	offset = offset + cmd_length:le_uint()
 
-		if arg == 'GET_WORKERS' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'GET_WORKERS'")		
-		
-		elseif arg == 'GET_IMAGES_LIST' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'GET_IMAGES_LIST'")
-
-		elseif arg == 'UPLOAD_IMAGE' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'UPLOAD_IMAGE'")
-			line, offset = add_sub_arg(line, offset, "Image Name: ")
-
-		elseif arg == 'DELETE_IMAGE' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'DELETE_IMAGE'")
-			line, offset = add_sub_arg(line, offset, "Image Name: ")
-
-		elseif arg == 'UPLOAD_IMAGE_WORKER' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'UPLOAD_IMAGE_WORKER'")
-			line, offset = add_sub_arg(line, offset, "Image Name: ")
-			line, offset = add_sub_arg(line, offset, "Worker ID: ")
-		
-		elseif arg == 'DELETE_IMAGE_WORKER' then
-			subtree:add(buffer(offset, arg:len()), "Command: 'DELETE_IMAGE_WORKER'")
-			line, offset = add_sub_arg(line, offset, "Image Name: ")
-			line, offset = add_sub_arg(line, offset, "Worker ID: ")
-
-		else
-			return
-		end
+	if command:string() ~= "UPLOAD_IMAGE" and command:string() ~= "DELETE_IMAGE" and command:string() ~= "UPLOAD_IMAGE_WORKER" 
+		and command:string() ~= "DELETE_IMAGE_WORKER" and command:string() ~= "RUN_NOW" and command:string() ~= "STOP_NOW" then
+		return
 	end
+
+	if buffer:len() < offset + 4 then
+		pinfo.desegment_len = 4
+		return
+	end
+
+	local image_name_length = buffer(offset,4)
+	subtree:add(buffer(offset, 4), "Image name length: "..image_name_length:le_uint())
+	offset = offset + 4
+
+	if buffer:len() < offset + image_name_length:le_uint() then
+		pinfo.desegment_len = image_name_length:le_uint()
+		return
+	end
+
+	local image_name = buffer(offset,image_name_length:le_uint())
+	subtree:add(buffer(offset,image_name_length:le_uint()), "Image name: "..image_name:string())
+	offset = offset + image_name_length:le_uint()
+
+	if command:string() ~= "UPLOAD_IMAGE_WORKER" and command:string() ~= "DELETE_IMAGE_WORKER" 
+		and command:string() ~= "RUN_NOW" and command:string() ~= "STOP_NOW" then
+		return
+	end
+
+	if buffer:len() < offset + 4 then
+		pinfo.desegment_len = 4
+		return
+	end
+
+	local worker_id_length = buffer(offset,4)
+	subtree:add(buffer(offset, 4), "Worker identifier length: "..worker_id_length:le_uint())
+	offset = offset + 4
+
+	if buffer:len() < offset + worker_id_length:le_uint() then
+		pinfo.desegment_len = worker_id_length:le_uint()
+		return
+	end
+
+	local worker_id = buffer(offset,worker_id_length:le_uint())
+	subtree:add(buffer(offset,worker_id_length:le_uint()), "Worker identifier: "..worker_id:string())
+	offset = offset + worker_id_length:le_uint()
+	
 end
 
 tcp_table = DissectorTable.get("tcp.port")
